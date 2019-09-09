df25cdf780b57247f6a4865d517f835d7eb6d81b
cRaeber <54545269+cRaeber@users.noreply.github.com>
2019-09-08 20:10:33 -0500

    Add files via upload

---
+++ A1/HW_1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string result;
+
+            do
+            {
+                result = DisplayMenu();
+                Run(result);
+            }
+            while (result != "5");
+
+            Console.WriteLine(" Good Bye...");
+        }
+
+        //creates the menu where the user will select what function they want to run or if they want to exit the program
+        public static string DisplayMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Homework 1");
+            Console.WriteLine("");
+            Console.WriteLine("Hit [1] to run ConvertDegrees.");
+            Console.WriteLine("Hit [2] to run ComputePolyOne.");
+            Console.WriteLine("Hit [3] to run ComputePolyTwo.");
+            Console.WriteLine("Hit [4] to run ConvertSeconds.");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Hit [5]: Exit;");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            var result = Console.ReadLine();
+            return result;
+        }
+
+        private static bool Run(string exeArg)
+        {
+
+            switch (exeArg.ToLower())
+            {
+
+                case "1":
+                    DegreesConversion();
+                    return true;
+
+                case "2":
+                    Polynomial(exeArg, 2);
+                    return true;
+
+                case "3":
+                    Polynomial(exeArg, 3);
+                    return true;
+
+                case "4":
+                    TimeConversion();
+                    return true;
+                case "5":
+                    Console.WriteLine("Exiting the Program!");
+                    return true;
+                default:
+                    Console.WriteLine("Please enter a value between 1-5");
+                    return true;
+            }
+        }
+
+        public static string DegreesConversion()
+        {
+            Console.WriteLine("Enter the temerature you want to convert:");
+            double f = Double.Parse(Console.ReadLine());
+            double c = (5 * (f - 31)) / 9;
+            string answer = ("The tempature in Celcus is " + c + " Degrees");
+            return answer;
+        }
+
+        //use an if to send it through the right poly function 
+        static string Polynomial(string arg, int form)
+        {
+            Console.WriteLine("Enter a value for x:");
+            double x = Double.Parse(Console.ReadLine());
+
+            if (form == 2)
+            {
+                double y = (3 * Math.Pow(x, 2)) - (7 * x) + 2;
+                string answer = ("The value of the polynomial is " + y);
+                return answer;
+            }
+            else
+            {
+                double y = (4 * Math.Pow(x, 2)) + (3* x) - 5;
+                string answer = ("The value of the polynomial is " + y);
+                return answer;
+            }                
+        }       
+
+        static string TimeConversion()
+        {
+            Console.WriteLine("Enter the number of seconds:");
+            var time = Console.ReadLine();
+            double sec = Double.Parse(time);
+            int hours;
+            int minuets;
+
+            hours = (int)Math.Floor((sec / 60) / 60);
+            sec = sec - ((hours * 60) * 60);
+
+            minuets = (int)Math.Floor(sec / 60);
+            sec = sec - (minuets * 60);
+
+            string answer = (time + " seconds converts to " + hours + " hours " + minuets + " minutes " + sec + " seconds");
+            return answer;
+        }
+    }
+}
---
+++ A1/Homework_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuSelection;
+            double userInput = 0.0;
+
+            DegreesConversion(userInput);
+            Polynomial(userInput);
+            Polynomial2(userInput);
+            TimeConversion(userInput);
+        }
+
+        static double DegreesConversion(double f)
+        {
+            double c = (5 * (f - 31)) / 9;
+            //need to write a line
+            "The tempature in Celcus is " + c + " Degrees"
+            return c;
+        }
+
+        static double Polynomial(double x)
+        {
+            double y = (3 * Math.Pow(x, 2)) - (7 * x) + 2;
+            //need to write a line
+            "The value of the polynomial is " + y
+            return y;
+        }
+
+        static double Polynomial2(double x)
+        {
+            double y = (4 * Math.Pow(x, 2)) + (3 * x) - 5;
+            //need to write a line
+            "The value of the polynomial is " + y
+            return y;
+        }
+
+        static double TimeConversion(double sec)
+        {
+            double time = sec;
+            int hours;
+            int minuets;
+            int seconds;
+
+            hours = (int)Math.Floor((sec / 60) / 60);
+            sec = sec - ((hours * 60) * 60);
+
+            minuets = (int)Math.Floor(sec / 60);
+            sec = sec - (minuets * 60);
+
+
+            (time + " seconds converts to " + hours + " hours " + minuets + " minutes " + sec + " seconds")
+            //need to write a line
+            return time;
+        }
+
+
+
+    }
+}
